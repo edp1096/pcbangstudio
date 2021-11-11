6dbe714541bb4c272686bb2c0443c1598e622c2d
Robert Sung-wook Shin <edp1096@users.noreply.github.com>
2021-11-11 21:38:22 +0900

    Add update_golang.cmd

---
+++ setup/assets/etc/update_golang.cmd
@@ -0,0 +1,27 @@
+@echo off
+
+echo This will remove existing Go binaries.
+echo If you not want update, Ctrl+C or close this cmd window
+echo.
+pause
+
+cd ..\tools\langs
+
+rmdir go /s /q
+
+For /f %%i in ('..\utils\gnuwin\curl.exe "https://golang.org/VERSION?m=text"') do echo. && echo Download %%i && set "gover=https://golang.org/dl/%%i.windows-amd64.zip"
+
+..\utils\gnuwin\curl.exe --progress-bar -Lo go.zip %gover%
+
+echo. && echo Extracting.. && echo.
+
+..\utils\gnuwin\unzip.exe -q go.zip
+del go.zip
+
+echo. && echo "Check go version" && echo.
+go\bin\go version
+
+cd ..\..\etc
+
+echo. && echo "Update Go finished" && echo.
+pause
